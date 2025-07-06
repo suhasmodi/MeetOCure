6c17dc6ed508a1d5e98e97dc7a217fa4bc4ba5f0
Soham Suryawanshi <suryawanshisoham7@gmail.com>
2025-07-06 14:53:28 +0530

    Cards

--- frontend-ankit/src/pages/doctor/DoctorPatientDetailsPage.jsx
+++ frontend-ankit/src/pages/doctor/DoctorPatientDetailsPage.jsx
@@ -79,7 +79,10 @@ const DoctorPatientDetailsPage = () => {
         <div className="bg-white p-6 rounded-xl shadow">
           <FaClock className="text-[#0A4D68] text-2xl mb-2 mx-auto" />
           <p className="text-base text-gray-500">Appointment Time</p>
-          <p className="text-lg font-semibold">{appt.time}</p>
+          <p className="text-lg font-semibold">
+            {new Date(appt.date).toISOString().split("T")[0]}
+            {appt.time}
+          </p>
         </div>
       </div>
 
