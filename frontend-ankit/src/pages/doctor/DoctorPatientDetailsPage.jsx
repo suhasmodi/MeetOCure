6c8ab84d6560fac62bd5b39f80a957b1a70dae93
Soham Suryawanshi <suryawanshisoham7@gmail.com>
2025-07-06 15:00:22 +0530

    Date Format

--- frontend-ankit/src/pages/doctor/DoctorPatientDetailsPage.jsx
+++ frontend-ankit/src/pages/doctor/DoctorPatientDetailsPage.jsx
@@ -74,7 +74,7 @@ const DoctorPatientDetailsPage = () => {
         <div className="bg-white p-6 rounded-xl shadow">
           <FaCalendarAlt className="text-[#0A4D68] text-2xl mb-2 mx-auto" />
           <p className="text-base text-gray-500">Appointment Date</p>
-          <p className="text-lg font-semibold">{new Date(appt.date).toISOString().split("T")[0]}
+          <p className="text-lg font-semibold">{new Date(appt.date).toLocaleDateString()}
             {appt.date}
           </p>
         </div>
