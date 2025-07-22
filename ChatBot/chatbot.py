0b20f88919cd77499a45de8d973a25ec007884df
Soham Suryawanshi <suryawanshisoham7@gmail.com>
2025-07-22 13:34:16 +0530

    Chabot

--- ChatBot/chatbot.py
+++ ChatBot/chatbot.py
@@ -121,7 +121,7 @@ Give the answer in HTML format using <b>, <h4>, <h5>, and bullet points using '-
 # ------------------- Flask Server API ------------------- #
 
 app = Flask(__name__)
-CORS(app)
+CORS(app, origins=["https://meet-o-cure-3pznp8uhd-oneterabyte7s-projects.vercel.app"], supports_credentials=True)
 
 assistant = MedicalAssistant()
 
