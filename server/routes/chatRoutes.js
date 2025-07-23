5b69f6cfdc9e2c6e425c60ea371e10cb6e50fcdc
Soham Suryawanshi <suryawanshisoham7@gmail.com>
2025-07-23 12:28:30 +0530

    Enhanced Chatbot

--- server/routes/chatRoutes.js
+++ server/routes/chatRoutes.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const { chatWithFlaskAI } = require("../controllers/chatController");
+const { chatWithFlaskAI } = require("../controllers/chatCOntroller");
 
 router.post("/chat", chatWithFlaskAI);
 
