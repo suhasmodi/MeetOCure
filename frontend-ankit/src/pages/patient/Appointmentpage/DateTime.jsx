b762f5d315ffca9cc316fc6578be0a1a897d70fb
Soham Suryawanshi <suryawanshisoham7@gmail.com>
2025-07-25 21:19:11 +0530

    Changes:appointmentController

--- frontend-ankit/src/pages/patient/Appointmentpage/DateTime.jsx
+++ frontend-ankit/src/pages/patient/Appointmentpage/DateTime.jsx
@@ -74,7 +74,7 @@ const DateTime = () => {
     try {
       const token = localStorage.getItem("token");
       const response = await axios.post(
-        "https://meetocure.onrender.com/api/patient/appointments",
+        "https://meetocure.onrender.com/api/bookAppointments",
         {
           doctorId: "686777ed62ea595b0eb8bc29",
           date: selectedDate,
